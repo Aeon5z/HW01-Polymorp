bdf537397a4192e33d05919d6bcaabce60621209
Aeonz <Aeonz@Aeonzs-Mac.local>
2017-09-24 13:35:27 -0700

    Initial Commit

---
+++ HW01-Polymorp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HW01-Polymorp
+//
+//  Created by Aeonz on 9/24/17.
+//  Copyright © 2017 Aeonz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
